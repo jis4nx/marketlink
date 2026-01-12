9eb8dd66d3187910af9afb9e8a5a55ea59eb5c8e
jis4nx <jis4nx@gmail.com>
2026-01-12 14:44:12 +0600

    :zap: Add migrations and UserRegistration View

--- user/views.py
+++ user/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from user.serializers.user_serializer import UserRegistrationSerializer, UserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework import status
+from user.models import User
 
-# Create your views here.
+
+class UserRegisterView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+
+            return Response(
+                {
+                    "user": UserSerializer(user).data,
+                    "tokens": {
+                        "refresh": str(refresh),
+                        "access": str(refresh.access_token),
+                    },
+                },
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
