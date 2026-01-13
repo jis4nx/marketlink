3c1d6289fd49cbd484317e549ec632b5eca93e78
jis4nx <jis4nx@gmail.com>
2026-01-13 23:01:35 +0600

    :zap: Add role in required field

--- user/serializers/user_serializer.py
+++ user/serializers/user_serializer.py
@@ -1,28 +1,33 @@
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
-from user.models import User 
+from user.models import User
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password]
+    )
     password2 = serializers.CharField(write_only=True, required=True)
-    
+
     class Meta:
         model = User
-        fields = ('email', 'password', 'password2', 'first_name', 'last_name', 'role')
+        fields = ("email", "password", "password2", "first_name", "last_name", "role")
         extra_kwargs = {
-            'first_name': {'required': False},
-            'last_name': {'required': False},
+            "first_name": {"required": False},
+            "last_name": {"required": False},
+            "role": {"required": True},
         }
-    
+
     def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."}
+            )
         return attrs
-    
+
     def create(self, validated_data):
-        validated_data.pop('password2')
-        password = validated_data.pop('password')
+        validated_data.pop("password2")
+        password = validated_data.pop("password")
         user = User.objects.create_user(password=password, **validated_data)
         return user
 
@@ -30,5 +35,5 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'first_name', 'last_name', 'role', 'date_joined')
-        read_only_fields = ('id', 'date_joined')
+        fields = ("id", "email", "first_name", "last_name", "role", "date_joined")
+        read_only_fields = ("id", "date_joined")
